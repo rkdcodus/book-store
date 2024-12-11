a51e7bb25085193b9798f130d99d6059b280baee
rkdcodus <rcd7275@naver.com>
2024-12-11 16:56:51 +0900

    feat(app.js): morgan 객체 생성 및 설정

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config();
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.listen(process.env.PORT, () => console.log("서버가 돌아갑니다"));
