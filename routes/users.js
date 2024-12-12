6adca3cd812ff5911302c928049234bb03c1936b
rkdcodus <rcd7275@naver.com>
2024-12-12 14:14:22 +0900

    feat(users.js): 회원가입 api 구현

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,41 @@
 const express = require("express");
+const conn = require("../mariadb");
+const { body, validationResult } = require("express-validator");
+
 const router = express.Router();
 
 router.use(express.json());
 
+function validator(req, res, next) {
+  const err = validationResult(req);
+
+  if (err.isEmpty()) return next();
+
+  res.status(400).json(err.array());
+}
+
 // 회원가입
-router.post("/signin", (req, res) => {
-  res.json("회원가입");
-});
+router.post(
+  "/join",
+  [
+    body("email").notEmpty().isEmail().withMessage("이메일 입력 필요"),
+    body("name").notEmpty().isString().withMessage("이름 입력 필요"),
+    body("password").notEmpty().withMessage("비밀번호 입력 필요"),
+    validator,
+  ],
+  (req, res) => {
+    const { email, name, password } = req.body;
+    const sql = "INSERT INTO users (email, name, password) VALUES (?,?,?);";
+
+    conn.query(sql, [email, name, password], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(400).end();
+      }
+    });
+    res.status(201).json({ message: `${name}님 회원가입을 축하합니다.` });
+  }
+);
 
 // 로그인
 router.post("/login", (req, res) => {
