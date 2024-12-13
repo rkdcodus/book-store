bd9cac30fadc4bc3f1656fa00a34e102d571a162
rkdcodus <rcd7275@naver.com>
2024-12-13 22:24:27 +0900

    feat(BookController.js): 도서 전체 조회 API, 카테고리별 도서 조회 API 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,7 +2,28 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const getBooks = (req, res) => {
-  res.json("도서 전체 조회");
+  const { category } = req.query;
+
+  if (category === undefined) {
+    const sql = "SELECT * FROM books";
+
+    conn.query(sql, (err, results) => {
+      res.status(StatusCodes.OK).json(results);
+    });
+
+    return;
+  }
+
+  const sql = "SELECT * FROM books WHERE category_id = ?";
+
+  conn.query(sql, category, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const getBook = (req, res) => {
