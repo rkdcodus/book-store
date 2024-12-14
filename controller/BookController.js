e00e0418a90ff7df21912b253a1a7382196f2725
rkdcodus <rcd7275@naver.com>
2024-12-14 23:04:18 +0900

    feat(BookController.js): 신간 도서 조회 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,12 +2,12 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const getBooks = (req, res) => {
-  const { category } = req.query;
+  const querySize = Object.keys(req.query).length;
+  const { category, news } = req.query;
+  let sql =
+    "SELECT books.id, category as category, title, form, author, isbn, pages, summary, detail, contents, price, pub_date, img FROM bookstore.books LEFT OUTER JOIN categories ON books.category_id = categories.id ";
 
-  if (category === undefined) {
-    const sql =
-      "SELECT books.id, category as category, title, form, author, isbn, pages, summary, detail, contents, price, pub_date, img FROM bookstore.books LEFT OUTER JOIN categories ON books.category_id = categories.id;";
-
+  if (!querySize) {
     conn.query(sql, (err, results) => {
       res.status(StatusCodes.OK).json(results);
     });
@@ -15,8 +15,12 @@ const getBooks = (req, res) => {
     return;
   }
 
-  const sql =
-    "SELECT books.id, category as category, title, form, author, isbn, pages, summary, detail, contents, price, pub_date, img FROM bookstore.books LEFT OUTER JOIN categories ON books.category_id = categories.id where books.category_id = ?;";
+  let sqlQuery = [];
+
+  if (category) sqlQuery.push("books.category_id = ? ");
+  if (news) sqlQuery.push("pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW() ");
+
+  sql += "where " + sqlQuery.join("and ");
 
   conn.query(sql, category, (err, results) => {
     if (err) {
