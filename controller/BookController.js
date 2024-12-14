1aad9b0840da8906eb4312cc49ab1e55c315bd7a
rkdcodus <rcd7275@naver.com>
2024-12-14 23:23:57 +0900

    feat(BookController.js): 도서 목록 조회 페이징 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 
 const getBooks = (req, res) => {
   const querySize = Object.keys(req.query).length;
-  const { category, news } = req.query;
+  const { category, news, limit, currentPage } = req.query;
+  let sqlQuery = [];
   let sql =
     "SELECT books.id, category as category, title, form, author, isbn, pages, summary, detail, contents, price, pub_date, img FROM bookstore.books LEFT OUTER JOIN categories ON books.category_id = categories.id ";
 
@@ -15,14 +16,14 @@ const getBooks = (req, res) => {
     return;
   }
 
-  let sqlQuery = [];
-
-  if (category) sqlQuery.push("books.category_id = ? ");
+  if (category) sqlQuery.push(`books.category_id = ${category} `);
   if (news) sqlQuery.push("pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW() ");
 
-  sql += "where " + sqlQuery.join("and ");
+  if (sqlQuery.length) sql += "where " + sqlQuery.join("and ");
 
-  conn.query(sql, category, (err, results) => {
+  if (limit && currentPage) sql += `LIMIT ${+limit} OFFSET ${limit * (currentPage - 1)}`;
+
+  conn.query(sql, (err, results) => {
     if (err) {
       console.error(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
