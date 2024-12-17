ae629885042421c2fc6fd07a47bab6d8c6fa0db5
rkdcodus <rcd7275@naver.com>
2024-12-17 21:31:11 +0900

    feat(CartController.js): 장바구니 조회 api 구현

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,3 +1,4 @@
+const { Result } = require("express-validator");
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
@@ -6,11 +7,35 @@ const selectBooks = (req, res) => {
 };
 
 const getCarts = (req, res) => {
-  res.json("장바구니에 담은 도서 조회");
+  const { userId } = req.body;
+  const sql = ` SELECT orders.id as orderId, book_id as bookId, selected, title, summary, price, quantity 
+                FROM orders 
+                LEFT JOIN books 
+                ON orders.book_id = books.id 
+                WHERE user_id = ?`;
+
+  conn.query(sql, userId, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const addToCart = (req, res) => {
-  res.json("장바구니 담기");
+  const { userId, bookId, quantity } = req.body;
+  const sql = "INSERT INTO orders ( user_id, book_id, quantity) VALUES (?,?,?)";
+
+  conn.query(sql, [userId, bookId, quantity], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    res.status(StatusCodes.CREATED).json(results);
+  });
 };
 
 const removeBookFromCart = (req, res) => {
