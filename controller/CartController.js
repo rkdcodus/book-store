16b094b46a63de4d88200ce8dbd1d9f7d5eeb7a5
rkdcodus <rcd7275@naver.com>
2025-02-06 15:49:48 +0900

    feat: jwt ReferenceError 오류시 401 코드 전달

--- controller/CartController.js
+++ controller/CartController.js
@@ -71,6 +71,8 @@ const addToCart = (req, res) => {
       .json({ message: "로그인 세션이 만료되었습니다. 다시 로그인 해주세요" });
   } else if (decodedJwt instanceof JsonWebTokenError) {
     return res.status(StatusCodes.BAD_REQUEST).json({ message: "잘못된 토큰입니다." });
+  } else if (decodedJwt instanceof ReferenceError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ message: "로그인이 필요합니다." });
   }
 
   const sql = "INSERT INTO orders ( user_id, book_id, quantity) VALUES (?,?,?)";
